133eb0d4b73d074b76ebbcfb4685192263ecc281
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-21 11:26:34 +0100

    added components

--- ToDoListChallenge/src/App.jsx
+++ ToDoListChallenge/src/App.jsx
@@ -1,10 +1,50 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [tasks, setTasks] = useState([]);
 
-  return <></>;
-}
+  const handleAddTask = (task) => {
+    let newTasksArr = [...tasks];
+    newTasksArr.push(task);
+    setTasks(newTasksArr);
+  };
+
+  const handleRemoveTask = (index) => {
+    let newTasksArr = [...tasks];
+    newTasksArr.splice(index, 1);
+    setTasks(newTasksArr);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>My To Do List</h1>
+        {tasks.map((myTasks, index) => {
+          return (
+            <Tasks
+              key={index}
+              myTasksData={myTasks}
+              removeTask={() => handleRemoveTask(index)}
+            />
+          );
+        })}
+      </div>
+
+      <div>
+        <h2>To Do:</h2>
+      </div>
+    </>
+  );
+};
+
+const Tasks = ({ myTasksData, removeTask }) => {
+  return (
+    <div>
+      <p>{myTasksData.task}</p>
+      <button onClick={removeTask}>Delete</button>
+    </div>
+  );
+};
 
 export default App;
