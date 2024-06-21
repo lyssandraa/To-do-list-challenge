0f4287c6ce4d99806d409ba6228db69f65afca4f
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-21 13:42:24 +0100

    added css

--- ToDoListChallenge/src/Tasks.jsx
+++ ToDoListChallenge/src/Tasks.jsx
@@ -1,12 +1,8 @@
 const Tasks = ({ task, removeTask, toggleTask }) => {
   return (
-    <div>
+    <div className={`task ${task.completed ? "completed" : ""}`}>
       <input type="checkbox" checked={task.completed} onChange={toggleTask} />
-      <span
-        style={{ textDecoration: task.completed ? "line-through" : "none" }}
-      >
-        {task.task}
-      </span>
+      <span>{task.task}</span>
       <button onClick={removeTask}>Delete</button>
     </div>
   );
